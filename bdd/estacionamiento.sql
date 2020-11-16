a0da6f653bb1a32352bfafea70c97cae85fc8291
Fernando Araujo <44034465+AlienatedFac@users.noreply.github.com>
2020-11-16 18:47:55 -0500

    Update estacionamiento.sql

--- bdd/estacionamiento.sql
+++ bdd/estacionamiento.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2020 a las 17:09:23
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.1
+-- Tiempo de generación: 17-11-2020 a las 00:47:36
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -44,10 +44,22 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
+  `Correo` varchar(100) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
   `hora_entrada` varchar(100) NOT NULL,
   `hora_salida` varchar(100) NOT NULL,
   `lugar` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `Correo`, `imagen`, `hora_entrada`, `hora_salida`, `lugar`) VALUES
+(20170057, 'Fernando', 'Araujo', 'substratum21@gmail.com', 'imagen/201700057.png', '10:30', '12:30', 20),
+(201700000, 'Juan', 'Perez Perez', 'juan@gmail.com', 'imagen/201700000.png', '1:30', '3:30', 21),
+(201700054, 'Sergio Raul', 'Aguilar Mendoza', 'sergio_raul@gmail.com', 'imagen/201700054.png', '10:40', '2:40', 22),
+(202000000, 'Pedro', 'Reyes', 'uwuxd@gmail.com', 'imagen/202000000.png', '1:30', '3:30', 5);
 
 --
 -- Índices para tablas volcadas
@@ -80,7 +92,7 @@ ALTER TABLE `lugares`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202000001;
 
 --
 -- Restricciones para tablas volcadas
