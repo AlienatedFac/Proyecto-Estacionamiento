32bd799cfa92e36b7c4030241a33bb6ab72a8bf4
Sergio1310 <40905681+Sergio1310@users.noreply.github.com>
2020-11-21 14:34:21 -0500

    fndo

--- usuarios.php
+++ usuarios.php
@@ -30,6 +30,7 @@ $var5="img/usuario.png";
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Usuarios</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/usuarios.css">
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
